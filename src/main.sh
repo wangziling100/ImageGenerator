c34a93ae4efedcaaaaa0a95673664bdcde174165
Bob Wang <wangziling1000@gmail.com>
2020-05-11 13:56:51 +0200

    <format input> |<format input> <traverse dir> |<traverse dir> if checked file or dir is effective |<traverse dir>

--- src/main.sh
+++ src/main.sh
@@ -5,12 +5,18 @@
 # This script finds out the effective parts of project files, and then minimizes the project through deleting the ineffective part
 
 # command main.sh dirname
-#@git <format input>
+#@git: <format input>
 root_dir=$1
 if [[ "${root_dir:${#root_dir}-1:1}" != "/" ]];then
     root_dir=${root_dir}/
 fi
 
-#@git <traverse dir>
+#@git: <traverse dir>
+read_dir(){
+    for f in 'ls $root_dir'; do
+        #@git: if checked file or dir is effective
+
+    done
+}
 
 # delete selected file or subdir, and test if the project runs rightly
