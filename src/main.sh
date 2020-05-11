53c798bde73540b04977f844e96568455a875d6d
Bob Wang <wangziling1000@gmail.com>
2020-05-12 00:12:12 +0200

    <build env> |<build env>

--- src/main.sh
+++ src/main.sh
@@ -10,6 +10,8 @@ root_dir=$1
 if [[ "${root_dir:${#root_dir}-1:1}" != "/" ]];then
     root_dir=${root_dir}/
 fi
+#@git: <build env>
+docker run -v $HOME/workspace:/workspace -it --name image-gen alpine bash &
 
 mkdir /tmp/checked_file
 
@@ -30,11 +32,12 @@ read_dir(){
         dir0=$(dirname $abs_f)
         source ${script_root}test/test_function.sh
         result=$?
+        echo $result
         if [ $result -eq 1 ]; then
             echo branch1 $result
+            mv /tmp/checked_file/$f $dir0
         else
             echo branch2 $result
-            mv /tmp/checked_file/$f $dir0
         fi
     done
 }
@@ -42,3 +45,5 @@ read_dir $root_dir
 
 # delete selected file or subdir, and test if the project runs rightly
 rm -r /tmp/checked_file
+docker container stop image-gen
+docker container rm image-gen
