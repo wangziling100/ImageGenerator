7966975a44b9bc3d8ec18d365904823b86ac2280
Bob Wang <wangziling1000@gmail.com>
2020-05-11 19:56:37 +0200

    <traverse dir> |<traverse dir> if checked file or dir is effective |<traverse dir>

--- src/main.sh
+++ src/main.sh
@@ -11,13 +11,28 @@ if [[ "${root_dir:${#root_dir}-1:1}" != "/" ]];then
     root_dir=${root_dir}/
 fi
 
+mkdir -p /tmp/checked_file
+
 #@git: <traverse dir>
 read_dir(){
     for f in `ls $root_dir`; do
         #@git: if checked file or dir is effective
-        echo $f
+        mv $f /tmp/checked_file
+        abs_f=${root_dir}$f
+        echo $abs_f
+        dir0=$(dirname $abs_f)
+        base=$(basename $abs_f)
+        source ${ws}test/test_function.sh
+        result=$?
+        echo $result
+        if [ $result -eq 1 ]; then
+            :
+        else
+            mv /tmp/checked_file/$base $dir0
+        fi
     done
 }
 read_dir $root_dir
 
 # delete selected file or subdir, and test if the project runs rightly
+rm -r /tmp/checked_file
